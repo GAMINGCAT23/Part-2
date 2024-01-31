fe1b49da41de677f5d0733a34a57dd7241d5e692
Prof Kit Barry <147667279+ProfKitBarry@users.noreply.github.com>
2024-01-30 19:15:44 -0500

    Week4 Task1 finishedUpdate README.md

---
+++ Assets/Week 4/Scripts/Plane.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Plane : MonoBehaviour
+{
+    public List<Vector2> points;
+    public float newPositionThreshold = 0.2f;
+    Vector2 LasPosition;
+    LineRenderer lineRenderer;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 1;
+        lineRenderer.SetPosition(0, transform.position);
+    }
+
+    private void OnMouseDown()
+    {
+        points = new List<Vector2>();
+        lineRenderer.positionCount = 1;
+        lineRenderer.SetPosition(0, transform.position);
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector2 newPosinion = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if(Vector2.Distance(LasPosition, newPosinion) > newPositionThreshold)
+        {
+            points.Add(newPosinion);
+            lineRenderer.positionCount++;
+            lineRenderer.SetPosition(lineRenderer.positionCount - 1, newPosinion);
+            LasPosition = newPosinion;
+        }
+    }
+}
