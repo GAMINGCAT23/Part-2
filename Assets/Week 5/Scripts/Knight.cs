b6852b659fb3d784f1cd23e926c0180ef2397487
GAMINGCAT23 <w2071458017@gmail.com>
2024-02-08 10:28:49 -0500

    Task 1 finished: get knight moving and finish few animation

---
+++ Assets/Week 5/Scripts/Knight.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+    Vector2 destination;
+    Vector2 movement;
+    public float speed = 3;
+    Rigidbody2D rigidbody;
+    Animator animator;
+    bool clickingOnSelf = false;
+    public float health;
+    public float maxHealth = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        health = maxHealth;
+    }
+
+    private void FixedUpdate()
+    {
+        movement = destination - (Vector2) transform.position;
+        if(movement.magnitude < 0.1)
+        {
+            movement = Vector2.zero;
+        }
+        rigidbody.MovePosition(rigidbody.position +  movement.normalized * speed * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && !clickingOnSelf)
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        animator.SetFloat("Movement", movement.magnitude);
+    }
+
+    private void OnMouseDown()
+    {
+       clickingOnSelf = true;
+       takeDamage(1);
+    }
+
+    private void OnMouseUp()
+    {
+        clickingOnSelf = false;
+    }
+
+    void takeDamage(float damage) 
+    {
+        health -= damage;
+        health = Mathf.Clamp(health, 0, maxHealth);
+        if (health == 0)
+        {
+            animator.SetTrigger("Death");
+        }
+        else 
+        {
+            animator.SetTrigger("TakeDamage");
+        }
+    }
+}
