866b62816aa26eba502a48bd274d1bc3de3c63c1
GAMINGCAT23 <w2071458017@gmail.com>
2024-02-22 10:21:52 -0500

    finish task1 b

--- Assets/Week 7/Scripts/player.cs
+++ Assets/Week 7/Scripts/player.cs
@@ -6,15 +6,40 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    SpriteRenderer spriteRenderer;
+    public Color DefultColor;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = DefultColor;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnMouseDown()
+    {
+        selected(true);
+    }
+
+    //private void OnMouseUp()
+    //{
+        //selected(false);
+    //}
+
+    public void selected(bool playerBeenSelected) 
+    {
+        if (playerBeenSelected)
+        {
+            spriteRenderer.color = Color.green;
+        }
+        else
+        {
+            spriteRenderer.color = DefultColor;
+        }
     }
 }
