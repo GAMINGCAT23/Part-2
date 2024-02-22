5e36c027d87338538dd2b8e51a0a95ed5db5f887
GAMINGCAT23 <w2071458017@gmail.com>
2024-02-22 11:30:38 -0500

    Task2 finish

--- Assets/Week 7/Scripts/Controller.cs
+++ Assets/Week 7/Scripts/Controller.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    public Slider ChargeSlider;
+    float chargeValue;
+    public float MaxCharge = 1;
+    Vector2 direction;
     public static player selectedPlayer { get; private set; }
     public static void SetSelectedPlayer(player player)
     {
@@ -15,5 +19,36 @@ public class Controller : MonoBehaviour
         }
         selectedPlayer = player;
         selectedPlayer.selected(true);
+    }
+    private void FixedUpdate()
+    {
+        if(direction != Vector2.zero)
+        {
+            selectedPlayer.Move(direction);
+            direction = Vector2.zero;
+            chargeValue = 0;
+            ChargeSlider.value = chargeValue;
+        }
+    }
+
+    private void Update()
+    {
+        if (selectedPlayer == null) return;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            chargeValue = 0;
+            direction = Vector2.zero;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            chargeValue += Time.deltaTime;
+            chargeValue = Mathf.Clamp(chargeValue, 0, MaxCharge);
+            ChargeSlider.value = chargeValue;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            direction = ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)selectedPlayer.transform.position).normalized * chargeValue;
+        }
     }
 }
--- Assets/Week 7/Scripts/player.cs
+++ Assets/Week 7/Scripts/player.cs
@@ -8,10 +8,13 @@ public class player : MonoBehaviour
 {
     SpriteRenderer spriteRenderer;
     public Color DefultColor;
+    Rigidbody2D rb;
+    public float speed = 100;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.color = DefultColor;
         selected(false);
@@ -42,5 +45,10 @@ public class player : MonoBehaviour
         {
             spriteRenderer.color = DefultColor;
         }
+    }
+
+    public void Move(Vector2 direction)
+    {
+        rb.AddForce(direction * speed);
     }
 }
